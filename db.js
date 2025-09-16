ef45417d63afc970e1c8e3c0d98e4d78d1431c3b
DavidSilva15 <147518658+DavidSilva15@users.noreply.github.com>
2025-09-15 22:23:54 -0300

    Update db.js

--- db.js
+++ db.js
@@ -1,12 +1,14 @@
 const mysql = require('mysql2/promise');
+
 const pool = mysql.createPool({
-    host: 'localhost',
-    user: 'root',
-    password: 'Bestdavidx23',
-    database: 'barbearia',
-    waitForConnections: true,
-    connectionLimit: 10,
-    queueLimit: 0,
-    // timezone: 'Z' // ajuste se necessário
+  host: process.env.DB_HOST || '127.0.0.1',
+  user: process.env.DB_USER || 'barbearia',
+  password: process.env.DB_PASS || 'SENHA_FORTE_AQUI',
+  database: process.env.DB_NAME || 'barbearia',
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
+  // timezone: 'Z'
 });
+
 module.exports = pool;
